259fe6f839ddc10993bf2d70df3307a92ef27545
Kynan <kynan.ly@hotmail.ca>
2020-05-06 16:33:56 -0600

    Updates

---
+++ precache-manifest.2bbf40722c006fbb158a1e83236918e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "226748801a78b0b830ee206a809fdd60",
+    "url": "/FlattenTheCurve/index.html"
+  },
+  {
+    "revision": "293a377e2d633480b686",
+    "url": "/FlattenTheCurve/static/css/2.75edc52f.chunk.css"
+  },
+  {
+    "revision": "ab68ecbf9f489105ba82",
+    "url": "/FlattenTheCurve/static/css/main.7d33ef2b.chunk.css"
+  },
+  {
+    "revision": "293a377e2d633480b686",
+    "url": "/FlattenTheCurve/static/js/2.1a5aac48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FlattenTheCurve/static/js/2.1a5aac48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab68ecbf9f489105ba82",
+    "url": "/FlattenTheCurve/static/js/main.05e46830.chunk.js"
+  },
+  {
+    "revision": "d0285bea4275ed3e2210",
+    "url": "/FlattenTheCurve/static/js/runtime-main.2d79d958.js"
+  }
+]);
