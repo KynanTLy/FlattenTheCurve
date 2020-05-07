0f3990c5ef6ca322273bf8754e20bb56152462e1
Kynan <kynan.ly@hotmail.ca>
2020-05-06 18:38:19 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FlattenTheCurve/precache-manifest.399938c63ebed6aaa472c1b77c99f28d.js"
+  "/FlattenTheCurve/precache-manifest.2a02858ec296083937adca14ade7f064.js"
 );
 
 self.addEventListener('message', (event) => {
